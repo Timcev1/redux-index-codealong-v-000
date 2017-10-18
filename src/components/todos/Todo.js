b35bb95f445342c5b9a34ed14fb7f485833b7763
Timothy Cevallos <timcev@gmail.com>
2017-10-18 15:17:43 +0000

    automatically backed up by learn

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,18 +1,11 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
+ 
 class Todo extends Component {
-
   render() {
-    const todos = this.props.store.getState().todos.map((todo, index) => {
-    return <li key={index}>{todo.text}</li>
-    });
-
-    return(
-      <ul>
-        {todos}
-      </ul>
+    return (
+      <li>{this.props.text}</li>
     );
   }
 };
-
+ 
 export default Todo;
